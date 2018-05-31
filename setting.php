df250d310548e7c82481b3efa89a147ffbace817
Cybertk <32348809+tolga000@users.noreply.github.com>
2018-05-31 06:12:45 +0300

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "7QN4PfYf7mytNmcgMBMxu7sGZ+fXZt3/NZjJM+rgDhTskOHPvye4DmFF5MlZ+sxKLW9vDANxpHqeHVhkezg+3B0HyR8i/E1QEfwv/eDfWGonwtdBT5KpOi4zy+Fxwuh0Io8MyRVi07IongPm1nQLoAdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "cd8f4572f3622290cda10474f89b8a52";
 		return $channelSecret;
 	}
 	public function getApiReply(){
